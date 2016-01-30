73a02ea51874d97ac63b938e959535dcdcfda490
kshitija <kkarkar@mail.csuchico.edu>
2016-01-29 17:55:59 -0800

    final working program with rand function

--- GameDie.cpp
+++ GameDie.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <cstdlib>
+#include <time.h>
 
+using namespace std;
 //class constructor that seeds the random number generator
 GameDie::GameDie()
 {
+   srand(time(NULL));
 }
 
 //generate a random number between 1-6 (inclusive) and display it
 void GameDie::roll()
 {
+   int i=rand()%7;
+   if(i!=0)
+      cout<<" Your number is : "<<i<<endl;
 }
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "GameDie.h"
+
+using namespace std;
+
+int main()
+{
+   GameDie g;
+   g.roll();
+   return 0;
+}
